fef64f2ca76d0770e8eb71bea4a744bf05f3ccfc
Zach <zvincent@nd.edu>
2025-03-19 15:16:49 -0400

    added temp pages for contact, about, changed project data order

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import Nav from "./Components/Shared/Nav/Nav.jsx";
 import Home from "./Components/Home/Home.jsx";
 import Projects from "./Components/Projects/Projects.jsx";
+import About from "./Components/About/About.jsx";
+import Contact from "./Components/Contact/Contact.jsx";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
@@ -11,6 +13,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="projects/:projectId" element={<Projects />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
       </Router>
     </>
---
+++ src/Components/About/About.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { useParams, useNavigate } from "react-router-dom";
+
+const About = () => {
+  const history = useNavigate();
+
+  const buttonHandler = () => {
+    history("/");
+  };
+
+  return (
+    <>
+      <button className="project-back" onClick={buttonHandler}>
+        Go back
+      </button>
+      <h1>This page is still under construction.</h1>
+    </>
+  );
+};
+
+export default About;
